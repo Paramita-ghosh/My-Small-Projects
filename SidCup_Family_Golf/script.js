c40be4e2a5e28f5a9764e27e109f0c483ce1aaf1
paramitathecdrom <gparamita576@gmail.com>
2025-09-06 21:38:46 +0000

    container config done

---
+++ SidCup_Family_Golf/script.js
@@ -0,0 +1,34 @@
+let cursor = document.querySelector("#cursor")
+let cursorb = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove", function(dets){
+    cursor.style.left = dets.x+"px"
+    cursor.style.top = dets.y+"px"
+    cursorb.style.left = dets.x-250+"px"
+    cursorb.style.top = dets.y-250+"px"
+})
+
+gsap.to("#nav",{
+    backgroundColor :"#000", 
+    duration:1,
+    height:"110px",
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        //markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1
+    }
+})
+
+gsap.to("#main",{
+    backgroundColor : "#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        markers:true,
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+    }
+})
