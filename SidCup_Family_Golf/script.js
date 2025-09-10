ee59156de30286e151fb5845dadba1793fe43711
paramitathecdrom <gparamita576@gmail.com>
2025-09-10 22:06:19 +0000

    container config done

--- SidCup_Family_Golf/script.js
+++ SidCup_Family_Golf/script.js
@@ -7,6 +7,21 @@ document.addEventListener("mousemove", function(dets){
     cursorb.style.top = dets.y-250+"px"
 })
 
+let h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(e){
+    e.addEventListener("mouseenter", function(){
+        cursor.style.scale = 2
+        cursor.style.border = "0.1px solid #fff"
+        cursor.style.backgroundColor = "transparent"
+    })
+
+    e.addEventListener("mouseleave", function(){
+        cursor.style.scale = 1
+        cursor.style.border = "0px solid #95C11E"
+        cursor.style.backgroundColor = "#95C11E"
+    })
+})
+
 gsap.to("#nav",{
     backgroundColor :"#000", 
     duration:1,
@@ -29,6 +44,73 @@ gsap.to("#main",{
         markers:true,
         start:"top -25%",
         end:"top -70%",
+        scrub:2
+    }
+})
+
+gsap.from("#about-us img, #about-us-in", {
+    y:90,
+    opacity:0,
+    duration:1,
+    stagger:0.4,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:2
+    }
+})
+
+gsap.from(".card", {
+    scale:0.8,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:2
+    }
+})
+
+gsap.from("#colon1", {
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        //markers:"true",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:3
+    }
+})
+
+gsap.from("#colon2", {
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon2",
+        scroller:"body",
+        //markers:"true",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:3
+    }
+})
+
+gsap.from("#page4 h1", {
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller:"body",
+        //markers:"true",
+        start:"top 75%",
+        end:"top 70%",
         scrub:2
     }
 })
